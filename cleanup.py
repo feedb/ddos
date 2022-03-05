c30f674fa5a6bd270af843a94f2aa977c1b55c27
Mychajlo Chodorev <ralfeus@gmail.com>
2022-03-05 17:50:16 +0100

    cleanup targets

--- cleanup.py
+++ cleanup.py
@@ -5,14 +5,21 @@ pattern = re.compile(r'^(?:https?:\/\/)?(?:[^@\/\n]+@)?([^:\/?\n]+)')
 
 source = open('targets.txt')
 dest = open('new_targets.txt', 'w')
+log = open('cleanup.log', 'w')
+domains = set()
 for line in tqdm(source.readlines()):
     domain_match = pattern.match(line)
     if domain_match:
         domain = domain_match.groups()[0]
-        try:
-            socket.gethostbyname(domain)
-            print(line, end='', file=dest)
-        except:
-            pass
+        if domain not in domains:
+            try:
+                socket.gethostbyname(domain)
+                print(line, end='', file=dest)
+                domains.add(domain)
+            except:
+                print(f'{domain} - couldn\'t resolve', file=log)
+        else:
+            print(f'{domain} - duplicate', file=log)
 source.close()
 dest.close()
+log.close()
